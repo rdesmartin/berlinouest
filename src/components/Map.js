e33e1cf0c257932a4548752b2fc449b8dcea337b
Remi <remi@MacBook-Air-de-Remi.local>
2018-08-11 01:48:39 +0200

    display berlin wall in map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,8 @@
+//@flow
 import React, { Component } from "react";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+
+var geojson = require('../assets/BerlinWall.json'); // eslint-disable-line import/no-webpack-loader-syntax
 
 export default class SimpleExample extends Component {
   constructor(props){
@@ -8,22 +11,36 @@ export default class SimpleExample extends Component {
       lat: 0,
       lng: 0,
       zoom: 13,
+      geojson: null,
     }
   }
 
+  getData() {
+    this.setState({
+      geojson
+  });
+  }
+
   componentDidMount() {
+    this.getData();
     this.setState((state) => this.props);
-    console.log(this.state);
   }
 
   render() {
-    const position = [this.state.lat, this.state.lng]
+    console.log(this.state.geojson);
+    const position = [this.state.lat, this.state.lng];
+    const { geojson } = this.state;
+
     return (
       <Map center={position} zoom={this.state.zoom} style={{height: "500px"}}>
         <TileLayer
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+      {
+        geojson &&
+          <GeoJSON data={geojson} />
+      }
         <Marker position={position}>
           <Popup>
             <p>You are here</p>
