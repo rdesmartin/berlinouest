e23f1a5af9de3d5e723d5770b5a1939e7d618e4f
Remi <remi@MacBook-Air-de-Remi.local>
2018-08-11 03:26:58 +0200

    add about page

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -7,9 +7,15 @@ class AboutView extends Component {
     return (
       <div>
         <h1>About Page</h1>
-          <p>
-          <Link to={{ pathname: '/home' }}>Home</Link>
-          </p>
+        <ul>
+          <li><Link to={{ pathname: '/home' }}>Home</Link></li>
+          <li><Link to={{ pathname: '/' }}>Map</Link></li>
+        </ul>
+        <p>This program uses React-Leaflet, React.js and Openstreetmap data to
+          tell if you are currently in West Berlin or not.</p>
+        <a href="https://github.com/rdesmartin/leaflet_reactjs_openstreetmap_berliner_mauer">
+          View project on GitHub
+        </a>
       </div>
     );
   }
