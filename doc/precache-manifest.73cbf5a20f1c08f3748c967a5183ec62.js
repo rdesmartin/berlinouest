c5415e4c024fc9c7468ef89d685a3651adca3aeb
Remi Desmartin <rdesmartin@gmail.com>
2019-07-30 18:06:30 +0200

    add production build in /doc

---
+++ doc/precache-manifest.73cbf5a20f1c08f3748c967a5183ec62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6795df018d2c10336cd94f1a689f54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa626a29a8457f6d1a46",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "51f0d4f77bafda5247ff",
+    "url": "/static/js/2.d0a92244.chunk.js"
+  },
+  {
+    "revision": "fa626a29a8457f6d1a46",
+    "url": "/static/js/main.255d764e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
